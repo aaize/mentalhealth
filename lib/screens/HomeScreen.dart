6a0d5e23e61a60ee2b84a9a98c4f56d3951c652e
aaize <abdulamreena@gmail.com>
2025-03-17 23:55:47 +0530

    mentalhealth

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -18,7 +18,7 @@ import 'hscreens/PodcastScreen.dart';
 import 'hscreens/VideosScreen.dart';
 import 'hscreens/WorkshopScreen.dart';
 import 'package:mentalhealth/screens/hscreens/QuestionnaireScreen.dart';
-
+import 'package:mentalhealth/screens/hscreens/Prof.dart';
 class HomeScreen extends StatefulWidget {
   final String userEmail; // The email of the logged-in user
   const HomeScreen({Key? key, required this.userEmail}) : super(key: key);
